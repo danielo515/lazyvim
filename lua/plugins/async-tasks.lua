c6f2a9dc0a40f3ad1f8a0e25af07344ce7de698f
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-05 15:21:55 +0200

    chore: properly setup asynctasks

--- lua/plugins/async-tasks.lua
+++ lua/plugins/async-tasks.lua
@@ -1,17 +1,18 @@
+--@type lazy.PluginSpec
 return {
-  -- I provide this dependency separately so packer stops annoying me about removing it
   "skywind3000/asyncrun.vim",
+  cmd = { "AsyncTask", "AsyncTaskList" },
   {
     "skywind3000/asynctasks.vim",
     requires = {
       "skywind3000/asyncrun.vim",
     },
-    setup = function()
-      vim.cmd [[
+    init = function()
+      vim.cmd([[
           let g:asyncrun_open = 8
           let g:asynctask_template = '~/.config/lvim/task_template.ini'
           let g:asynctasks_extra_config = ['~/.config/lvim/tasks.ini']
-          ]]
+          ]])
     end,
   },
 }
