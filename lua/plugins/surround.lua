7dc3f1c6e04ddba563811e7492a7d5caa9685d46
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-05 10:49:43 +0200

    feat: surronds for reason

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,8 +1,26 @@
+vim.filetype.add({
+  extension = {
+    re = function(path, bufnr)
+      vim.b.minisurround_config = {
+        custom_surroundings = {
+          a = {
+            input = { "%[%|().-()%|%]" },
+            output = { left = "[|", right = "|]" },
+          },
+          j = {
+            input = { "%{%j%|().-()%|%j%}" },
+            output = { left = "{j|", right = "|j}" },
+          },
+        },
+      }
+    end,
+  },
+})
 return {
   "echasnovski/mini.surround",
   opts = {
     mappings = {
-      add = "gsa",
+      add = "ys",
       delete = "gsd",
       find = "gsf",
       find_left = "gsF",
