e085f36ceab3aad1497845eb85b013a29b889b6f
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-10 11:02:48 +0200

    lualine: buffer lines

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,5 +4,10 @@ return {
   opts = function(_, opts)
     table.insert(opts.sections.lualine_x, "filetype")
     table.insert(opts.sections.lualine_x, "searchcount")
+    opts.sections.lualine_z = {
+      function()
+        return vim.api.nvim_buf_line_count(0)
+      end,
+    }
   end,
 }
