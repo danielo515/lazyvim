f0fe3502d3118bb0313ab8f9f086f4e28506b3f7
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-11 12:50:23 +0200

    feat: added some of my keymaps from lunar

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,19 +5,29 @@ require("danielo.globals")
 local nmap = require("user.keymap").nmap
 local vmap = require("user.keymap").vmap
 local del = function(...)
-  return pcall(vim.api.nvim_del_keymap, ...)
+  return pcall(del, ...)
 end
 del("n", "<leader>ww")
 del("n", "<leader>wd")
 del("n", "<leader>w-")
 del("n", "<leader>w|")
+
+--Cleanups
+del("i", "jj")
+del("i", "kj")
+del("i", "jk")
+del("n", "gp")
+del("n", "<A-k>")
+del("n", "<M-k>")
+del("n", "kj")
+del("n", "jk")
+
 nmap("<C-C>", function()
   vim.api.nvim_win_close(0, false)
 end, "Close current window")
 
 nmap("<Tab>", function()
   require("telescope.builtin").buffers({
-
     layout_strategy = "vertical",
     layout_config = { height = 0.65, mirror = true, prompt_position = "top", anchor = "N" },
   })
@@ -45,6 +55,35 @@ nmap(",c", ":cNext!<cr>", "Prev item in quickfix")
 nmap(",n", "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next error")
 nmap(",s", ":s/\\v", "Search/replace local line", false)
 vmap(",s", ":s/\\v", "Search/replace local line", false)
+
+local map = vim.keymap.set;
+-- Normal mode mappings
+map("n", "<S-l>", ":BufferLineCycleNext<CR>", { desc = "Cycle to next buffer" })
+map("n", "<S-h>", ":BufferLineCyclePrev<CR>", { desc = "Cycle to previous buffer" })
+map("n", "<C-F>", "<cmd>lua require('user.find').fzf_find()<cr>", { desc = "Find files using FZF" })
+map("n", "<S-X>", "<cmd>BufferKill<cr>", { desc = "Close the current buffer" })
+map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename symbol using LSP" })
+map("n", "<M-cr>", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "Trigger LSP code action" })
+map("n", "<M-f>", "<cmd>lua require('fzf-lua').files()<cr>", { desc = "Find files using FZF-Lua" })
+map("n", "<Tab>", "<cmd>lua require('user.telescope').buffers()<cr>", { desc = "List open buffers using Telescope" })
+map("n", "<S-Tab>", ":Neotree float reveal<cr>", { desc = "Toggle Neotree file explorer" })
+map("n", "<C-N>", ":NvimTreeFindFile<cr>", { desc = "Find the current file in NvimTree" })
+map("n", "<C-;>", ":Telescope command_history<cr>", { desc = "Show command history using Telescope" })
+map("n", ",n", "<cmd>lua vim.diagnostic.goto_next()<cr>", { desc = "Go to the next diagnostic" })
+map("n", "+", "<cmd>3wincmd > <cr>", { desc = "Increase the current window width" })
+map("n", "-", "<cmd>3wincmd < <cr>", { desc = "Decrease the current window width" })
+map("n", "tk", ":tabclose<cr>", { desc = "Close the current tab" })
+map("n", "tn", ":tabnew<cr>", { desc = "Create a new tab" })
+map("n", "th", ":tabprev<cr>", { desc = "Navigate to the previous tab" })
+
+-- Visual mode mappings
+map("v", "s", "<cmd>Pounce<cr>", { noremap = true, silent = true })
+map("v", "<M-cr>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>", { noremap = true, silent = true })
+map("v", "p", '"_dP', { noremap = true, silent = true })
+
+-- Insert mode mappings
+map("i", "<C-f>", "<cmd>lua require('user.find').fzf_find()<cr>", { noremap = true, silent = true })
+map("i", "<C-u>", "<cmd>lua require('luasnip.extras.select_choice')()<cr>", { noremap = true, silent = true })
 
 -- Navigation
 vim.keymap.set("n", "s", ":Pounce<CR>", { noremap = true, silent = true, desc = "Extend visual selection with Pounce" })
