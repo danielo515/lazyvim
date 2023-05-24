4b6aceabb8b6d3a8bca3e370d31d3761f44f6959
Danielo Rodriguez <rdanielo@gmail.com>
2023-05-24 16:28:43 +0200

    cmp: tmux source

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,5 +1,6 @@
 return {
-  dependencies = { "hrsh7th/cmp-emoji", "lukas-reineke/cmp-rg" },
+  dependencies = { "lukas-reineke/cmp-rg", "hrsh7th/cmp-cmdline", "andersevenrud/cmp-tmux" },
+  event = { "InsertEnter", "CmdWinEnter" },
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
@@ -15,6 +16,14 @@ return {
           additional_arguments = "--smart-case --hidden",
         },
       }
+      ,
+      {
+        name = "tmux",
+        group_index = 1,
+        max_item_count = 5,
+        option = { all_panes = true, label = "[tmux]" },
+        priority_weight = 50
+      },
 
     }
     vim.list_extend(opts.sources, sources)
@@ -28,6 +37,32 @@ return {
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
     }
+
+    local cmdline_mappings = {
+      select_next_item = {
+        c = function(fallback)
+          if cmp.visible() then
+            return cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert } (fallback)
+          else
+            return cmp.mapping.complete { reason = cmp.ContextReason.Auto } (fallback)
+          end
+        end,
+      },
+      select_prev_item = {
+        c = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+      },
+    }
+    -- cmp.setup.cmdline("/", {
+    --   mapping = {
+    --     ["<Down>"] = cmdline_mappings.select_next_item,
+    --     ["<C-n>"] = cmdline_mappings.select_next_item,
+    --     ["<Tab>"] = cmdline_mappings.select_next_item,
+    --     ["<C-p>"] = cmdline_mappings.select_prev_item,
+    --     ["<Up>"] = cmdline_mappings.select_prev_item,
+    --     ["<S-Tab>"] = cmdline_mappings.select_prev_item,
+    --   },
+    --   sources = cmp.config.sources({ { name = "buffer" }, }, { { name = "cmdline_history" }, }),
+    -- })
     vim.print(opts)
   end,
 }
