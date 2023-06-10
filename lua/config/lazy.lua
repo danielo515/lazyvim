010edfbe36923229cfded716bfae1b306413f072
Danielo Rodriguez <rdanielo@gmail.com>
2023-06-10 03:46:11 +0200

    extra: fuck edgy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     -- import/override with your plugins
     { import = "plugins" },
   },
