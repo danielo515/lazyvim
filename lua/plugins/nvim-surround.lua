069234e31cf4d7ad20e85e61172cf764f9cda12d
Danielo Rodriguez <rdanielo@gmail.com>
2023-07-11 13:02:04 +0200

    plug: nvim surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,15 @@
+return {
+  "wellle/targets.vim",       -- Adds more targets to vim's built-in text object motions
+  {
+    "kylechui/nvim-surround", -- The best surround I tried so far
+    version = "*",            -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  { "mini.ai",       enabled = false },
+  { "mini.surround", enabled = false }
+}
