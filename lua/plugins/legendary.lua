9a91885826ed7095089e27d05e11b75d71eb0f22
Danielo Rodriguez <rdanielo@gmail.com>
2023-12-22 09:20:40 +0100

    plugin: legendary

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -5,5 +5,14 @@ return {
   -- its recommended to load legendary.nvim before other plugins
   priority = 10000,
   lazy = false,
-  opts = { lazy_nvim = { auto_register = true } },
+  opts = {
+    lazy_nvim = { auto_register = true },
+    which_key = { auto_register = true },
+    extensions = {
+      nvim_tree = true,
+      smart_splits = false,
+      op_nvim = false,
+      diffview = false,
+    },
+  },
 }
