6fde25e7a363aa82bbed76114d235661192546b9
Danielo Rodriguez <rdanielo@gmail.com>
2024-01-30 08:39:59 +0100

    conceallevel 0

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,5 +4,6 @@
 require("config.filetype")
 vim.o.winbar = "%{%v:lua.require'user.winbar'.statusline()%}"
 vim.opt.pumheight = 30 -- 0 will use all available space, but could cause performance issues
-vim.o.splitkeep = 'screen'
-vim.cmd('set noswapfile')
+vim.o.splitkeep = "screen"
+vim.cmd("set noswapfile")
+vim.o.conceallevel = 0
