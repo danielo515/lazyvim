582c39f180a5d5a7fc0da9a18b3fd0230cf6e774
Danielo Rodriguez <rdanielo@gmail.com>
2024-01-30 08:40:10 +0100

    lock

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("danielo.globals")
+require("danielo.globals") -- injects some global utils
 pcall(require, "danielo_nvim") -- this comes from local package haxe-nvim
